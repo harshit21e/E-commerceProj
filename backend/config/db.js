a9539fe9351022db174879afaa4850cb65c7490d
harshit21e <133681929+harshit21e@users.noreply.github.com>
2024-04-22 22:21:19 +0530

    Setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
+   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log(`Successfully connnected to mongoDB 👍`);
   } catch (error) {
