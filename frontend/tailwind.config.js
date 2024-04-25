0a5784975f8f09bdedb0d9f27489629dfde61f39
harshit21e <133681929+harshit21e@users.noreply.github.com>
2024-04-25 17:28:02 +0530

    Setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms')],
+}
